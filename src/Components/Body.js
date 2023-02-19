f399f09f115ceaae44cc06517566bf80280ecfd7
Nishant <Nishantshelar56@gmail,com>
2023-02-19 23:57:06 +0530

    Loaded data from swiggy api using React useEffect

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,8 +1,25 @@
 import { RestaurantList } from "../config";
 import RestaurantCard from "./RestaurantCard";
+import { useState,useEffect } from "react";
+
 
 const Body=()=>{
-    const searchTxt="KFC";
+    function filterData(searchTxt,restaurants){
+       return  restaurants.filter((restaurant)=>restaurant?.data?.name?.toLowerCase()?.includes(searchTxt?.toLowerCase()));
+    }
+    const [restaurants,setrestaurants]=useState(RestaurantList);
+    const [searchTxt,setSearchTxt]=useState("")
+
+    async function getRestauarants(){
+        const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.2183307&lng=72.9780897&page_type=DESKTOP_WEB_LISTING")
+        const json=await data.json();
+        setrestaurants(json?.data?.cards[2]?.data?.data?.cards);
+    }
+    useEffect(()=>{
+       getRestauarants()
+    },[])
+
+
     return(
     <>    
     <div className="search-container">
@@ -11,11 +28,18 @@ const Body=()=>{
         className="search-input"
         placeholder="Search"
         value={searchTxt}
-        /> 
+        onChange={(event)=>{
+            setSearchTxt(event.target.value);
+        }}
+        />
+        <button className="search-btn" onClick={()=>{
+            const data=filterData(searchTxt,restaurants);
+            setrestaurants(data);
+        }}>Search - {searchTxt}</button>  
     </div>    
     <div className="Restaurant-list">
         {
-            RestaurantList.map(restaurant=>{
+            restaurants.map(restaurant=>{
                 return <RestaurantCard {...restaurant.data} key={restaurant.data.id}/>
             })
         }
